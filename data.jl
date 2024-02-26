b056a84f6d74f5328e7fa4c8bb71bf26408f02bf
me <76593873+stunlocked1@users.noreply.github.com>
2024-02-26 22:49:43 +0300

    added transforms, split

--- data.jl
+++ data.jl
@@ -6,33 +6,38 @@ always_true(a::Any) = true
 struct Sample
     sample::Any
     loader::Function
+    transform::Function
     label::Any
-    Sample(sample::Any, loader::Function, label::Function) = new(sample, loader, label(sample))
-    Sample(sample::Any, loader::Function, label::Any) = new(sample, loader, label)
 end
+Sample(sample::Any, loader::Function, transform::Function, label::Function) = Sample(sample, loader, transform, label(sample))
 
 abstract type Dataset end
 mutable struct DatasetLabeled <: Dataset
     samples::Vector{Sample}
     labels::Vector{Any}
-    DatasetLabeled() = new([], [])
 end
+DatasetLabeled() = DatasetLabeled([], [])
 
 function Base.getindex(dataset::DatasetLabeled, index::Int64)
     sample = dataset.samples[index]
-    return (sample.loader(sample.sample), OneHotArrays.onehot(sample.label, dataset.labels))
+    return (sample.sample |> sample.loader |> sample.transform,  OneHotArrays.onehot(sample.label, dataset.labels))
 end
 
 function Base.getindex(dataset::DatasetLabeled, indexes::Vector{Int64})
     return MLUtils.batch([Base.getindex(dataset, index) for index in indexes]) # ignore
 end
 
-function Base.length(dataset::Dataset)
-    return length(dataset.samples)::Int
+function Base.length(collection::Dataset)
+    return length(collection.samples)::Int
 end
 
-function add_sample!(dataset::DatasetLabeled, sample::Any, loader::Function, label::Any)
-    sample = Sample(sample, loader, label)
+function Base.copy(x::DatasetLabeled)
+    return DatasetLabeled(copy(x.samples), copy(x.labels))
+end
+
+"Add sample to a dataset"
+function add_sample!(dataset::DatasetLabeled, sample::Any; loader::Function, transform::Function = identity, label::Any)
+    sample = Sample(sample, loader, transform, label)
     push!(dataset.samples, sample)
     if sample.label ∉ dataset.labels
         push!(dataset.labels, sample.label)
@@ -40,27 +45,68 @@ function add_sample!(dataset::DatasetLabeled, sample::Any, loader::Function, lab
 end
 
 "Add folder to a dataset."
-function add_folder!(dataset::DatasetLabeled, path::String; loader::Function, label::Any, filter::Function = always_true, extensions::Vector{String} = [])
-    for (root, _, files) in walkdir(path)
+function add_folder!(dataset::DatasetLabeled, path_to_folder::String; loader::Function, transform::Function = identity, label::Any, filter::Function = always_true, extensions::Vector{String} = [])
+    for (root, _, files) in walkdir(path_to_folder)
         for file in files
             p = joinpath(root, file)
             # if filter(p) and has one of the extensions / extensions list is empty
             if filter(p) && (!isempty(extensions) ? in(splitext(file)[2], extensions) : true)
-                add_sample!(dataset, p, loader, label)
+                add_sample!(dataset, p, loader=loader, transform=transform, label=label)
             end
         end
     end
 end
 
-"Get the last folder in a path. For example `datasets/images/car/car4.jpg` >> `car`"
+"Create a dataset from a folder."
+function DatasetLabeled(path_to_folder::String; loader::Function, transform::Function = identity, label::Any, filter::Function = always_true, extensions::Vector{String} = [])
+    ds = DatasetLabeled([], [])
+    add_folder!(ds, path_to_folder, loader = loader, transform = transform, label = label, filter = filter, extensions = extensions)
+    return ds
+end
+
+"Get the last folder in a path. For example `datasets/images/car/car4.jpg` -> `car`"
 function last_folder(path::String)
     return splitdir(splitdir(path)[1])[2]
 end
 
 "Pre-applies loader to all samples in the dataset."
 function preload!(dataset::Dataset)
-    dataset.samples = [Sample(sample.loader(sample.sample), identity, sample.label) for sample in dataset.samples]
+    dataset.samples = [Sample(sample.loader(sample.sample), identity, sample.transform, sample.label) for sample in dataset.samples]
 end
+
+function split_dataset(dataset::Dataset, splits::Vector{Float64})
+    # get dataset class
+    dataset_length = length(dataset)
+    
+    # make sure splits sum up to 1
+    if sum(splits) > 1
+        splits .= splits ./ sum(splits)
+    elseif sum(splits) < 1
+        push!(splits, 1 - sum(splits))
+    end
+
+
+    # get number of samples
+    n = length(dataset)
+    # get indexes of splits
+    indexes = Vector{Int64}(splits .* n)
+    split_datasets = []
+    cur = 1
+
+    println(indexes)
+    # get samples
+    for i in indexes
+        # get samples from current index to next
+        println(cur, ", ", i)
+        samples = dataset.samples[cur:min(cur+i, dataset_length)]
+        cur += i
+        ds = copy(dataset)
+        ds.samples = samples
+        push!(split_datasets, ds)
+    end
+    return split_datasets
+end
+
 # ---------------
 
 # include("./image.jl")
